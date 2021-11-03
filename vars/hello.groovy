3e8d16ec46615800bf244b8754e044c403c09b3b
Sam Manickaraj <sammanickaraj@hotmail.com>
2021-11-03 21:09:09 +0530

    print out

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,6 +1,6 @@
 def call(Map<String, String> config = [:]) {
     stage('Configuration Setup') {
         echo "Hi Sam!! from shared library v1.0"
-        echo config.appName
+        echo "Input App from Jenkinsfile: " config.appName
     }
 }
