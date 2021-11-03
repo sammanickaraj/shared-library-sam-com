ea297f860195ef40aeb67a5bb946a152158bf558
Sam Manickaraj <sammanickaraj@hotmail.com>
2021-11-03 21:02:55 +0530

    using map to collect input from jenkins file

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,21 +1,5 @@
-def call() {
-node {
-        stage('Configuration Setup') {
-            echo "Configuration setup"
-        }
-        stage('Testing Setup') {
-            echo "Testing Setup"
-        }
-        stage('SonarQube scan') {
-            echo "SonarQube Scan in progress"
-            echo "SonarQube scan"
-        }
-        stage('Copy Image') {
-            echo "Copy Image"
-            sh "mkdir /tmp/test_unique"
-        }
-         stage('Stage Image') {
-            echo "Staging image"
-        }
+def call(Map<String, String> config = [:]) {
+    stage('Configuration Setup') {
+        echo "Hi Sam!! from shared library v1.0"
     }
 }
