a0fd6e827815644c70f05dcbb617535d70725de3
Sam Manickaraj <sammanickaraj@hotmail.com>
2021-11-03 19:50:47 +0530

    Calling all stages in shared library

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,6 +1,22 @@
 def call() {
-    node {
-        stage('Checout')
-             echo "Hi Sam!! From Shared Library!!!!!"
-}
+node {
+        stage('Configuration Setup') {
+            echo "Configuration setup"
+            echo "${random_var}"
+        }
+        stage('Testing Setup') {
+            echo "${testing_var}"
+        }
+        stage('SonarQube scan') {
+            echo "SonarQube Scan in progress"
+            echo "${env_testvar}"
+        }
+        stage('Copy Image') {
+            echo "Copy Image"
+            sh "mkdir /tmp/test_unique"
+        }
+         stage('Stage Image') {
+            echo "Staging image"
+        }
     }
+}
