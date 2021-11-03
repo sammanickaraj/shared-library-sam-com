ec8e75a65ade87815fe9935ac7316422bb73cba0
Sam Manickaraj <sammanickaraj@hotmail.com>
2021-11-03 21:14:13 +0530

    Approver name

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,6 +1,13 @@
 def call(Map<String, String> config = [:]) {
     stage('Configuration Setup') {
         echo "Hi Sam!! from shared library v1.0"
+    }
+
+    stage('Print AppName') {
         echo "Input App from Jenkinsfile: " + config.appName
+    }
+
+    stage('Print Approver Name') {
+        echo "Input Approver name from Jenkinsfile: " + config.devApprovers
     }
 }
