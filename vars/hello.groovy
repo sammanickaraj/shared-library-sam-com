b4e79d0a5c01f6a164cc5d93e30843657bdc5c6f
Sam Manickaraj <sammanickaraj@hotmail.com>
2021-11-04 15:15:25 +0530

    Added new function to retrieve scm

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,13 +1,10 @@
 def call(Map<String, String> config = [:]) {
     stage('Configuration Setup') {
-        echo "Hi Sam!! from shared library v1.0"
+        collectSourceCodeInfoDev(config.projectFilePath)
     }
 
-    stage('Print AppName') {
-        echo "Input App from Jenkinsfile: " + config.appName
-    }
+}
 
-    stage('Print Approver Name') {
-        echo "Input Approver name from Jenkinsfile: " + config.devApprovers
-    }
+def collectSourceCodeInfoDev() {
+    checkout scm
 }
