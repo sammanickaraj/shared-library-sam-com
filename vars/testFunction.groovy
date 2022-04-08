a51c593ffafa27cef5c87f53772ad2bf100cceeb
sammanickaraj@hotmail.com <sammanickaraj@hotmail.com>
2022-04-08 16:00:56 +0530

    closure

--- vars/testFunction.groovy
+++ vars/testFunction.groovy
@@ -11,7 +11,7 @@ def call(Map<String, String> config = [:] ) {
 }
 
 def collectSourceCode() {
-    def statusJenkinsAgent(closure) {
+    def statusJenkinsAgent() {
         node(any) {
             checkout scm
         }
